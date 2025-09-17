3fb8c5f3e58934d7c8415ea4e8f29e594356114d
Rick <201260024+Urffin-Juss@users.noreply.github.com>
2025-09-17 12:53:55 +0300

    add func read trans from file

---
+++ src/trans_from_file.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+
+
+def reads_financial_transactions(file_path):
+    """считывает файл csv, выводит в виде списка словарей"""
+    with open(file_path, "r", encoding="UTF-8") as file:
+        reader = csv.DictReader(file)
+        transactions = list(reader)
+        return transactions
+
+
+def reads_financial_transactions_excel(file_path):
+    """считывает файл excel, выводит в виде списка словарей"""
+    excel_data = pd.read_excel(file_path, parse_dates=["date"])
+    transactions = excel_data.to_dict(orient="records")
+    return transactions
+
+
+
+if __name__ == "__main__":
+    result_1 = reads_financial_transactions("C:/Users/Thunderobot/Downloads/transactions.csv")
+    result_2 = reads_financial_transactions_excel("C:/Users/Thunderobot/Downloads/transactions_excel.xlsx")
+    print(result_1, result_2)
